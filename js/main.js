ed967e966506bd334a92befa7fa3e831be38bdc6
dtsitovich <d-tsitovich@ya.ru>
2021-05-14 16:02:02 +0300

    Added Files Structure & Home Page

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/* SEARCH BAR */
+
+var searchButton = document.querySelector(".search__btn");
+
+searchButton.onclick = function () {
+  var searchBar = document.querySelector(".search__bar");
+
+  if (searchBar.style.display === "none") {
+    searchBar.style.display = "flex";
+  } else {
+    searchBar.style.display = "none";
+  }
+};
